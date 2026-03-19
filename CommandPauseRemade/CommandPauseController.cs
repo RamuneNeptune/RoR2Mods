939d27cbfbb74394f698fca5d2ffa477c0c1689e
RamuneNeptune <94365980+RamuneNeptune@users.noreply.github.com>
2026-03-19 14:58:47 +1000

    CommandPauseRemade 1.0.1

--- CommandPauseRemade/CommandPauseController.cs
+++ CommandPauseRemade/CommandPauseController.cs
@@ -6,7 +6,8 @@ namespace Ramune.CommandPauseRemade
     {
         public static readonly BuffDef[] PauseBuffDefs = [RoR2Content.Buffs.HealingDisabled, RoR2Content.Buffs.Immune, DLC3Content.Buffs.Untargetable, RoR2Content.Buffs.Entangle];
 
-        public static readonly Dictionary<CharacterMaster, int> MasterSessionIds = [];
+        // Track the amount of times each master has opened a Command menu, used to deal with the mess of a player opening several menus within the protection delay time
+        public static readonly Dictionary<CharacterMaster, int> TrackedMasterInstances = [];
 
         public NetworkUIPromptController networkUIPromptController;
 
@@ -24,24 +25,30 @@ namespace Ramune.CommandPauseRemade
             {
                 trackedMaster.GetBody()?.RemoveBuff(RoR2Content.Buffs.Entangle);
 
+                var currentInstanceForMaster = TrackedMasterInstances.GetValueOrDefault(trackedMaster);
+
+                // If protection delay is > 0, start a coroutine that waits until after the delay to remove protections
                 if(ModConfig.ProtectionDelay.Value > 0f)
                 {
-                    RoR2Application.instance.StartCoroutine(StartDelay(trackedMaster, (MasterSessionIds.GetValueOrDefault(trackedMaster))));
+                    RoR2Application.instance.StartCoroutine(StartDelay(trackedMaster, currentInstanceForMaster));
                 }
-                else DisableProtection(trackedMaster, (MasterSessionIds.GetValueOrDefault(trackedMaster)));
+                // If protection delay is set to 0, skip starting a coroutine and just immediately remove protections
+                else DisableProtection(trackedMaster, currentInstanceForMaster);
             }
 
             trackedMaster = currentMaster;
 
             if(trackedMaster)
             {
-                MasterSessionIds[trackedMaster] = (MasterSessionIds.GetValueOrDefault(trackedMaster)) + 1;
+                // New menu instance opened
+                // +1 to the instance count for this trackedMaster
+                TrackedMasterInstances[trackedMaster] = (TrackedMasterInstances.GetValueOrDefault(trackedMaster)) + 1;
 
                 ToggleProtection(trackedMaster, true);
             }
         }
 
-
+        // Re-run Update() logic instead of re-using code here, and the Update() logic is specifically able to safely run without issues of null references
         public void OnDestroy() => Update();
 
 
@@ -55,17 +62,19 @@ namespace Ramune.CommandPauseRemade
         }
 
 
-        public static void DisableProtection(CharacterMaster master, int ticket)
+        public static void DisableProtection(CharacterMaster master, int currentInstanceForMaster)
         {
-            if(master && MasterSessionIds.GetValueOrDefault(master) == ticket)
+            // If currentInstanceForMaster matches the total instance count for this master, the player hasn't opened another menu within the protection delay time (all clear to remove protections)
+            // If they have opened another menu within the protection delay time, this returns early and waits for them to close the new menu before re-running this logic
+            if(master && TrackedMasterInstances.GetValueOrDefault(master) == currentInstanceForMaster)
                 ToggleProtection(master, false);
         }
 
 
-        public static IEnumerator StartDelay(CharacterMaster master, int ticket)
+        public static IEnumerator StartDelay(CharacterMaster master, int currentInstanceForMaster)
         {
             yield return new WaitForSeconds(ModConfig.ProtectionDelay.Value);
-            DisableProtection(master, ticket);
+            DisableProtection(master, currentInstanceForMaster);
         }
     }
 }
