1a07b782ea7529893ea639201c7d5b8580e9cd7e
RamuneNeptune <94365980+RamuneNeptune@users.noreply.github.com>
2026-03-19 12:06:09 +1000

    CommandPauseRemade update configuration

--- CommandPauseRemade/Config.cs
+++ CommandPauseRemade/Config.cs
@@ -1,8 +1,5 @@
 ﻿
 
-using RiskOfOptions.OptionConfigs;
-using RiskOfOptions.Options;
-
 namespace Ramune.CommandPauseRemade
 {
     public static class ModConfig
@@ -14,13 +11,29 @@ namespace Ramune.CommandPauseRemade
 
         public static void Init(ConfigFile config)
         {
-            EnableSingleplayer = config.Bind("Singleplayer", "Enable Singleplayer Pausing", true, "If true, time will freeze while opening the Artifact of Command menu in singleplayer.");
+            EnableSingleplayer = config.Bind("Singleplayer", "Enable Singleplayer Pausing", true, "If true, time will freeze while opening the Artifact of Command menu in singleplayer. (default: true)");
 
-            EnableMultiplayer = config.Bind("Multiplayer", "Enable Multiplayer Protection", true, "If true, players receive god mode and protection buffs while in the Artifact of Command menu in multiplayer.");
+            EnableMultiplayer = config.Bind("Multiplayer", "Enable Multiplayer Protection", true, "If true, players receive protection while in the Artifact of Command menu in multiplayer. (default: true)");
 
-            ProtectionDelay = config.Bind("Multiplayer", "Protection Removal Delay", 0.35f, "The delay (in seconds) before protections are removed after closing the Command menu.");
+            ProtectionDelay = config.Bind("Multiplayer", "Protection Removal Delay", 0.85f, "The delay (in seconds) before protections are removed after closing the Command menu. (default: 0.85)");
 
-            ModSettingsManager.SetModDescription("Pauses the game in singleplayer and provides protection for all players in multiplayer while using Artifact of Command menu.");
+            var iconPath = System.IO.Path.Combine(System.IO.Path.GetDirectoryName(typeof(ModConfig).Assembly.Location), "icon.png");
+
+            if (File.Exists(iconPath))
+            {
+                var iconTexture = new Texture2D(2, 2, TextureFormat.RGBA32, false);
+
+                if(iconTexture.LoadImage(File.ReadAllBytes(iconPath)))
+                {
+                    iconTexture.filterMode = FilterMode.Point;
+                    iconTexture.Apply();
+
+                    var iconSprite = Sprite.Create(iconTexture, new Rect(0, 0, iconTexture.width, iconTexture.height), new Vector2(0.5f, 0.5f));
+                    ModSettingsManager.SetModIcon(iconSprite);
+                }
+            }
+
+            ModSettingsManager.SetModDescription("While opening an Artifact of Command menu, pauses the game in Singeplayer, and provides protection to players in Multiplayer (Required host-only for MP)");
 
             ModSettingsManager.AddOption(new CheckBoxOption(EnableSingleplayer, true));
 
